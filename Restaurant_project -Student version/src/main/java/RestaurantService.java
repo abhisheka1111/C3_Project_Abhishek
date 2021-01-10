72954a5611125f81a959397bf1244c553dbbe3b1
abhisheka1111 <75586704+abhisheka1111@users.noreply.github.com>
2021-01-11 01:58:12 +0530

    Updated and edited

--- Restaurant_project -Student version/src/main/java/RestaurantService.java
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -6,7 +6,7 @@ public class RestaurantService {
     public static List<Restaurant> restaurants = new ArrayList<>();
 
     public Restaurant findRestaurantByName(String restaurantName){
-
+        //for loop for accessing and then returning restaurant if find is complete
         for(Restaurant restaurant: restaurants){
         if(restaurant.getName().equals(restaurantName)){
             return restaurant;
